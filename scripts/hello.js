aab4a97208d198c5151f911825da2fb75249844e
leoperezx <leoperez.x@gmail.com>
2021-04-18 11:38:25 -0500

    cree la funcion imagenDia

--- scripts/hello.js
+++ scripts/hello.js
@@ -17,20 +17,12 @@ const MESES = [
 
 const monthByNumber = (mes) => { return MESES[mes] }
 
-function saludo() {
-    const fecha = new Date()
-    const hora = fecha.getHours()
-    const numeroDia = fecha.getDay()
-    const dia = fecha.getDate()
-    const mes = fecha.getMonth()
-    const ano = fecha.getFullYear()
-
-    let p = 3
-    console.log(p, numeroDia)
-
+function imagenDia(numeroDelDia){
+    // Crear una funcion  que reciba el numeroDia y ejecute los cambios en la imagen
+    // Agregar una clase en la imagen para cada dia que debe mostrar
     const image = document.querySelector(".ventana img")
 
-    switch (p) {
+    switch (numeroDelDia) {
         case 0: image.style.top = '-270px'; break;
         case 1: image.style.top = '-10px'; break;
         case 2: image.style.top = '-54px'; break;
@@ -41,11 +33,20 @@ function saludo() {
         
     }
 
+}
+
+function saludo() {
+    const fecha = new Date()
+    const hora = fecha.getHours()
+    const numeroDia = fecha.getDay()
+    const dia = fecha.getDate()
+    const mes = fecha.getMonth()
+    const ano = fecha.getFullYear()
+
+    imagenDia(numeroDia)
+
     // Refactorizar esto en una funcion que reciba hora y devuelva los valores
-    const estiloHora = {
-        styleName: 'mañana',
-        saludo: 'Buenos días'
-    }
+    const estiloHora = { styleName: 'mañana', saludo: 'Buenos días' }
     if (hora < 13) { estiloHora.saludo = "Buenos días"; estiloHora.styleName = "mañana" }
     else if (hora < 19) { estiloHora.saludo = "Buenas tardes"; estiloHora.styleName = "tarde" }
     else if (hora < 24) { estiloHora.saludo = "Buenas noches"; estiloHora.styleName = "noche" }
@@ -53,6 +54,7 @@ function saludo() {
     document.querySelector(".hello").innerHTML = estiloHora.saludo
     document.querySelector(".today").innerHTML = `Hoy es ${dia} de ${monthByNumber(mes)} de ${ano}`
     document.body.classList.add(estiloHora.styleName)
+
 }
 
 
